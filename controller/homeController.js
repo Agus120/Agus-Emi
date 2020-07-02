40ba93c10679c116f19ece7c836b2e25045d39c1
Agus <aguszingariello@gmail.com>
2020-07-01 21:57:27 -0300

    espacios agregados

--- controller/homeController.js
+++ controller/homeController.js
@@ -1,16 +1,28 @@
 const fs = require('fs')
+
 const concesionarias = JSON.parse(fs.readFileSync('./data/concesionarias.json', 'utf-8'));
+
 const home ={
+
     index:function(req,res){
-        res.set({'content-type':'text/plain;charset=utf-8'})
-        res.write(' ----------\n')
-        res.write('|Bienvenido|\n')
-        res.write(' ----------\n\n')
-        res.write("Estas son nuestras concesionaras:\n")        
+
+        res.set({'content-type':'text/plain;charset=utf-8'});
+
+        res.write(' ----------\n');
+
+        res.write('|Bienvenido|\n');
+
+        res.write(' ----------\n\n');
+
+        res.write("Estas son nuestras concesionaras:\n");        
+
         concesionarias.forEach(sucursal => {
-            res.write(sucursal.sucursal + '\n')
+
+            res.write(sucursal.sucursal + '\n');
+
         });
-        res.end()
+
+        res.end();
     }
 }
 
--- routes/home.js
+++ routes/home.js
@@ -1,42 +1,9 @@
-/* const fs = require(`fs`);
-
-let homePage = {
-
-    bd: './data/concesionarias.json',
-
-    titulo: " Bienvenido ! ",
-
-    leerJSON: function() {
-
-        let concJson = fs.readFileSync(this.bd, 'utf-8');
-
-        let conces = JSON.parse(concJson);
-
-        return conces;
-    },
-
-    listarNombres: function() {
-
-        let conces = this.leerJSON();
-
-        let titleConces = []
-
-        conces.conces.forEach(function(conc) {
-            titleConces.push(conc.title);
-        })
-
-        titleConces.sort();
-
-        return titleConces;
-    }
-}
-
-module.exports = homePage; */
 const express = require('express');
+
 const router = express.Router();
 
-const homeController = require('../controller/homeController')
+const homeController = require('../controller/homeController');
 
-router.get('/', homeController.index)
+router.get('/', homeController.index);
 
-module.exports = router
+module.exports = router;
